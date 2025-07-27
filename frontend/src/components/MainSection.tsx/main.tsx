a4322a5e42fcbb556b6e54f4801b78672c316e1c
nikash12 <g.nikash13579@gmail.com>
2025-07-27 21:31:31 +0530

    initalized

---
+++ frontend/src/components/MainSection.tsx/main.tsx
@@ -0,0 +1,18 @@
+export default function Main(){
+    return(
+        <div>
+            <div>
+                <h1>Selected Project:</h1>
+                <div className="flex">
+                    <button>+</button>
+                    <label>Add new Module</label>
+                </div>
+                <div className="flex">
+                    <button>+</button>
+                    <label>Add new Module</label>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
